5001bd5e2577aa5f6a8d2a92f3944e712d7a0561
Pr0c3ss0r <pcfix911@hotmail.com>
2013-08-11 14:06:15 -0400

    windower.text.* change

--- menu/menu.lua
+++ menu/menu.lua
@@ -83,12 +83,12 @@ function RefreshMenu(MenuName)
    for i = 0, _Menu.list[MenuName]['mx'] do
       tb=tb.._Menu.list[MenuName]['Core_Text'][i+_Menu.list[MenuName]['scr']]
    end
-   tb_set_text('Menu_TB_'..MenuName, tb)
+   windower.text.set_text('Menu_TB_'..MenuName, tb)
 
-   x,y = tb_get_location('Menu_TB_'..MenuName)
+   x,y = windower.text.get_location('Menu_TB_'..MenuName)
 
-   tb_set_location('Menu_cur_'..MenuName, x, y + _Menu.list[MenuName]['cur'] * 14)
-   tb_set_text('Menu_cur_'..MenuName, _Menu.list[MenuName]['Core_Text'][_Menu.list[MenuName]['cur'] + _Menu.list[MenuName]['scr']])
+   windower.text.set_location('Menu_cur_'..MenuName, x, y + _Menu.list[MenuName]['cur'] * 14)
+   windower.text.set_text('Menu_cur_'..MenuName, _Menu.list[MenuName]['Core_Text'][_Menu.list[MenuName]['cur'] + _Menu.list[MenuName]['scr']])
 end
 
 function CreateMenu(MenuName, Caption, opt, x, y, mx, sub_key)
@@ -143,54 +143,56 @@ function CreateMenu(MenuName, Caption, opt, x, y, mx, sub_key)
 
    -- Create all Textbox
 
-   tb_create('Menu_Caption_'..MenuName)
-   tb_set_location('Menu_Caption_'..MenuName, x, y)
-   tb_set_bg_color('Menu_Caption_'..MenuName, 180, 80, 110, 140)
-   tb_set_color('Menu_Caption_'..MenuName, 250, 250, 250, 250)
-   tb_set_bold('Menu_Caption_'..MenuName, 'true')
-   tb_set_text('Menu_Caption_'..MenuName, Caption)
-   tb_set_bg_visibility('Menu_Caption_'..MenuName, true)
-   tb_set_font('Menu_Caption_'..MenuName, 'Courier New', 9)
-   tb_set_visibility('Menu_Caption_'..MenuName, true)
-
-   tb_create('Menu_TB_'..MenuName)
-   tb_set_location('Menu_TB_'..MenuName, x, y+15)
-   tb_set_bg_color('Menu_TB_'..MenuName, 140, 54, 43, 0)
-   tb_set_color('Menu_TB_'..MenuName, 250, 250, 250, 250)
-   tb_set_bold('Menu_TB_'..MenuName, 'true')
-   tb_set_text('Menu_TB_'..MenuName, tb)
-   tb_set_bg_visibility('Menu_TB_'..MenuName, true)
-   tb_set_font('Menu_TB_'..MenuName, 'Courier New', 9)
-   tb_set_visibility('Menu_TB_'..MenuName, true)
-
-   tb_create('Menu_cur_'..MenuName)
-   tb_set_bg_color('Menu_cur_'..MenuName, 155, 40, 40, 100)
-   tb_set_color('Menu_cur_'..MenuName, 255, 255, 255, 255)
-   tb_set_bold('Menu_cur_'..MenuName, 'true')
-   tb_set_text('Menu_cur_'..MenuName, _Menu.list[MenuName]['Core_Text'][_Menu.list[MenuName]['cur']])
-   tb_set_bg_visibility('Menu_cur_'..MenuName, true)
-   tb_set_font('Menu_cur_'..MenuName, 'Courier New', 9)
-   tb_set_visibility('Menu_cur_'..MenuName, false)
+   windower.text.create('Menu_Caption_'..MenuName)
+   windower.text.set_location('Menu_Caption_'..MenuName, x, y)
+   windower.text.set_bg_color('Menu_Caption_'..MenuName, 180, 80, 110, 140)
+   windower.text.set_color('Menu_Caption_'..MenuName, 250, 250, 250, 250)
+   windower.text.set_bold('Menu_Caption_'..MenuName, 'true')
+   windower.text.set_text('Menu_Caption_'..MenuName, Caption)
 
 
-   tb_create('Menu_scr_'..MenuName)
-   tb_set_bg_color('Menu_scr_'..MenuName, 140, 80, 80, 80)
-   tb_set_color('Menu_scr_'..MenuName, 255, 0, 0, 0)
-   tb_set_bold('Menu_scr_'..MenuName, 'true')
-   tb_set_text('Menu_scr_'..MenuName, scr)
-   tb_set_bg_visibility('Menu_scr_'..MenuName, true)
-   tb_set_font('Menu_scr_'..MenuName, 'Courier New', 9)
-   tb_set_visibility('Menu_scr_'..MenuName, false)
+   windower.text.set_bg_visibility('Menu_Caption_'..MenuName, true)
+   windower.text.set_font('Menu_Caption_'..MenuName, 'Courier New', 9)
+   windower.text.set_visibility('Menu_Caption_'..MenuName, true)
+
+   windower.text.create('Menu_TB_'..MenuName)
+   windower.text.set_location('Menu_TB_'..MenuName, x, y+15)
+   windower.text.set_bg_color('Menu_TB_'..MenuName, 140, 54, 43, 0)
+   windower.text.set_color('Menu_TB_'..MenuName, 250, 250, 250, 250)
+   windower.text.set_bold('Menu_TB_'..MenuName, 'true')
+   windower.text.set_text('Menu_TB_'..MenuName, tb)
+   windower.text.set_bg_visibility('Menu_TB_'..MenuName, true)
+   windower.text.set_font('Menu_TB_'..MenuName, 'Courier New', 9)
+   windower.text.set_visibility('Menu_TB_'..MenuName, true)
+
+   windower.text.create('Menu_cur_'..MenuName)
+   windower.text.set_bg_color('Menu_cur_'..MenuName, 155, 40, 40, 100)
+   windower.text.set_color('Menu_cur_'..MenuName, 255, 255, 255, 255)
+   windower.text.set_bold('Menu_cur_'..MenuName, 'true')
+   windower.text.set_text('Menu_cur_'..MenuName, _Menu.list[MenuName]['Core_Text'][_Menu.list[MenuName]['cur']])
+   windower.text.set_bg_visibility('Menu_cur_'..MenuName, true)
+   windower.text.set_font('Menu_cur_'..MenuName, 'Courier New', 9)
+   windower.text.set_visibility('Menu_cur_'..MenuName, false)
 
 
-   tb_create('Menu_bar_'..MenuName)
-   tb_set_bg_color('Menu_bar_'..MenuName, 255, 255, 255, 255)
-   tb_set_color('Menu_bar_'..MenuName, 255, 0, 0, 0)
-   tb_set_bold('Menu_bar_'..MenuName, 'true')
-   tb_set_text('Menu_bar_'..MenuName, ' \\cr')
-   tb_set_bg_visibility('Menu_bar_'..MenuName, true)
-   tb_set_font('Menu_bar_'..MenuName, 'Courier New', 9)
-   tb_set_visibility('Menu_bar_'..MenuName, false)
+   windower.text.create('Menu_scr_'..MenuName)
+   windower.text.set_bg_color('Menu_scr_'..MenuName, 140, 80, 80, 80)
+   windower.text.set_color('Menu_scr_'..MenuName, 255, 0, 0, 0)
+   windower.text.set_bold('Menu_scr_'..MenuName, 'true')
+   windower.text.set_text('Menu_scr_'..MenuName, scr)
+   windower.text.set_bg_visibility('Menu_scr_'..MenuName, true)
+   windower.text.set_font('Menu_scr_'..MenuName, 'Courier New', 9)
+   windower.text.set_visibility('Menu_scr_'..MenuName, false)
+
+
+   windower.text.create('Menu_bar_'..MenuName)
+   windower.text.set_bg_color('Menu_bar_'..MenuName, 255, 255, 255, 255)
+   windower.text.set_color('Menu_bar_'..MenuName, 255, 0, 0, 0)
+   windower.text.set_bold('Menu_bar_'..MenuName, 'true')
+   windower.text.set_text('Menu_bar_'..MenuName, ' \\cr')
+   windower.text.set_bg_visibility('Menu_bar_'..MenuName, true)
+   windower.text.set_font('Menu_bar_'..MenuName, 'Courier New', 9)
+   windower.text.set_visibility('Menu_bar_'..MenuName, false)
 
 
    -- kick this function every .01 sec. (no return or return false cancel the functin loop)
@@ -200,22 +202,22 @@ function CreateMenu(MenuName, Caption, opt, x, y, mx, sub_key)
       fn = function(key) 
          _Menu.callback_event[key].clock = os.clock() + .01
          key = string.gsub(key, 'onload_Menu.', '')
-         local x2,y2 = tb_get_extents('Menu_TB_'..key)
+         local x2,y2 = windower.text.get_extents('Menu_TB_'..key)
 
          if x2 == 0 then  -- No graphic update yet.
             return true
          end
 
-         local x,y = tb_get_location('Menu_TB_'..key)
+         local x,y = windower.text.get_location('Menu_TB_'..key)
 
-         tb_set_location('Menu_bar_'..key, x, y)
+         windower.text.set_location('Menu_bar_'..key, x, y)
          Move_Menu(key, x, y)
 
-         tb_set_visibility('Menu_Caption_'..key, true)
-         tb_set_visibility('Menu_TB_'..key, true)
-         tb_set_visibility('Menu_cur_'..key, true)
-         tb_set_visibility('Menu_scr_'..key, true)
-         tb_set_visibility('Menu_bar_'..key, true)
+         windower.text.set_visibility('Menu_Caption_'..key, true)
+         windower.text.set_visibility('Menu_TB_'..key, true)
+         windower.text.set_visibility('Menu_cur_'..key, true)
+         windower.text.set_visibility('Menu_scr_'..key, true)
+         windower.text.set_visibility('Menu_bar_'..key, true)
 
          if type(_Menu.list[key]['on_load']) == 'function' then
             _Menu.list[key]['on_load'](_Menu.list[key])
@@ -224,11 +226,11 @@ function CreateMenu(MenuName, Caption, opt, x, y, mx, sub_key)
 
    _Menu.list[MenuName]['close'] = function()
       write('closing menu')
-      tb_delete('Menu_TB_'..MenuName)
-      tb_delete('Menu_cur_'..MenuName)
-      tb_delete('Menu_scr_'..MenuName)
-      tb_delete('Menu_bar_'..MenuName)
-      tb_delete('Menu_Caption_'..MenuName)
+      windower.text.delete('Menu_TB_'..MenuName)
+      windower.text.delete('Menu_cur_'..MenuName)
+      windower.text.delete('Menu_scr_'..MenuName)
+      windower.text.delete('Menu_bar_'..MenuName)
+      windower.text.delete('Menu_Caption_'..MenuName)
       _Menu.list[MenuName] = nil
    end
 
@@ -247,20 +249,20 @@ function menu_combine(set1,set2)
 end
 
 function Move_Menu(MenuName, x, y)
-   local x1,y1 = tb_get_location('Menu_TB_'..MenuName)
-   local x2,y2 = tb_get_extents('Menu_TB_'..MenuName)
+   local x1,y1 = windower.text.get_location('Menu_TB_'..MenuName)
+   local x2,y2 = windower.text.get_extents('Menu_TB_'..MenuName)
 
    _Menu.list[MenuName]['x'] = x
    _Menu.list[MenuName]['y'] = y - 15
 
-   tb_set_location('Menu_Caption_'..MenuName, x, y - 15)
-   tb_set_location('Menu_TB_'..MenuName, x, y)
-   tb_set_location('Menu_cur_'..MenuName, x, y + _Menu.list[MenuName]['cur'] * 14)
+   windower.text.set_location('Menu_Caption_'..MenuName, x, y - 15)
+   windower.text.set_location('Menu_TB_'..MenuName, x, y)
+   windower.text.set_location('Menu_cur_'..MenuName, x, y + _Menu.list[MenuName]['cur'] * 14)
 
-   tb_set_location('Menu_scr_'..MenuName, x + x2, y)
-   local bar_x,bar_y = tb_get_location('Menu_bar_'..MenuName)
+   windower.text.set_location('Menu_scr_'..MenuName, x + x2, y)
+   local bar_x,bar_y = windower.text.get_location('Menu_bar_'..MenuName)
    bar_y = bar_y - y1
-   tb_set_location('Menu_bar_'..MenuName, x + x2, y + bar_y)
+   windower.text.set_location('Menu_bar_'..MenuName, x + x2, y + bar_y)
 end
 
 function menu_mouse_event(ActionType, x, y, is_blocked)
@@ -318,8 +320,8 @@ function menu_mouse_event(ActionType, x, y, is_blocked)
       _Menu.First_Click = 0
       if _Menu._MenuPress ~= nil then
 
-         x1,y1 = tb_get_location('Menu_TB_'.._Menu._MenuPress)
-         x2,y2 = tb_get_extents('Menu_TB_'.._Menu._MenuPress)
+         x1,y1 = windower.text.get_location('Menu_TB_'.._Menu._MenuPress)
+         x2,y2 = windower.text.get_extents('Menu_TB_'.._Menu._MenuPress)
 
          if _Menu._MenuPress_type == 'caption' or _Menu._MenuPress_type == 'content' then
             this = _Menu.list[_Menu._MenuPress]
@@ -336,8 +338,8 @@ function menu_mouse_event(ActionType, x, y, is_blocked)
             _Menu._MenuPressY = y
 
          elseif _Menu._MenuPress_type == 'scroll' then
-            xx1,yy1 = tb_get_location('Menu_bar_'.._Menu._MenuPress)
-            xx2,yy2 = tb_get_extents('Menu_bar_'.._Menu._MenuPress)
+            xx1,yy1 = windower.text.get_location('Menu_bar_'.._Menu._MenuPress)
+            xx2,yy2 = windower.text.get_extents('Menu_bar_'.._Menu._MenuPress)
 
             if _Menu._MenuPressY > yy1 and _Menu._MenuPressY < yy1 + yy2 then 
 
@@ -353,7 +355,7 @@ function menu_mouse_event(ActionType, x, y, is_blocked)
                   _Menu._MenuPressY = y
                end
 
-               tb_set_location('Menu_bar_'.._Menu._MenuPress, xx1, NewY)
+               windower.text.set_location('Menu_bar_'.._Menu._MenuPress, xx1, NewY)
 
                local MaxScr = (#_Menu.list[_Menu._MenuPress]['text'] - _Menu.list[_Menu._MenuPress]['mx'])
 
@@ -376,8 +378,8 @@ function menu_mouse_event(ActionType, x, y, is_blocked)
 
 
    for key, val in pairs(_Menu.list) do
-      x1,y1 = tb_get_location('Menu_TB_'..key)
-      x2,y2 = tb_get_extents('Menu_TB_'..key)
+      x1,y1 = windower.text.get_location('Menu_TB_'..key)
+      x2,y2 = windower.text.get_extents('Menu_TB_'..key)
 
       if x < x1 + x2+9 and x > x1 then 
          if y < y1 + y2 and y > y1 - 15 then 
@@ -421,11 +423,11 @@ windower.register_event('mouse', menu_mouse_event)
 
 function menu_event_unload()
    for key, val in pairs(_Menu.list) do
-      tb_delete('Menu_TB_'..key)
-      tb_delete('Menu_cur_'..key)
-      tb_delete('Menu_scr_'..key)
-      tb_delete('Menu_bar_'..key)
-      tb_delete('Menu_Caption_'..key)
+      windower.text.delete('Menu_TB_'..key)
+      windower.text.delete('Menu_cur_'..key)
+      windower.text.delete('Menu_scr_'..key)
+      windower.text.delete('Menu_bar_'..key)
+      windower.text.delete('Menu_Caption_'..key)
       _Menu.list[key] = nil
    end
 end
